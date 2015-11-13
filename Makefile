712cc12fe284c53f64a527f49fc560129d1847be
IlGuru <s.busnelli@tin.it>
2015-11-13 19:49:03 +0100

    Codelite project

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ RigolCsvToWav - Debug ]----------"
-	@cd "RigolCsvToWav" && "$(MAKE)" -f  "RigolCsvToWav.mk"
+	@echo "----------Building project:[ RigolCsvToWav - Release64 ]----------"
+	@cd "RigolCsvToWav" && $(MAKE) -f  "RigolCsvToWav.mk"
 clean:
-	@echo "----------Cleaning project:[ RigolCsvToWav - Debug ]----------"
-	@cd "RigolCsvToWav" && "$(MAKE)" -f  "RigolCsvToWav.mk" clean
+	@echo "----------Cleaning project:[ RigolCsvToWav - Release64 ]----------"
+	@cd "RigolCsvToWav" && $(MAKE) -f  "RigolCsvToWav.mk" clean
--- RigolCsvToWav/RigolCsvToWav.mk
+++ RigolCsvToWav/RigolCsvToWav.mk
@@ -2,21 +2,21 @@
 ## Auto Generated makefile by CodeLite IDE
 ## any manual changes will be erased      
 ##
-## Debug
+## Release64
 ProjectName            :=RigolCsvToWav
-ConfigurationName      :=Debug
+ConfigurationName      :=Release64
 WorkspacePath          := "C:\cygwin64\home\sbusnelli\progetti_c\libsndfile_001\RigolCsvToWav"
 ProjectPath            := "C:\cygwin64\home\sbusnelli\progetti_c\libsndfile_001\RigolCsvToWav\RigolCsvToWav"
-IntermediateDirectory  :=./Debug
+IntermediateDirectory  :=./Release64
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
 User                   :=sbusnelli
-Date                   :=11/12/15
+Date                   :=11/13/15
 CodeLitePath           :="C:\Program Files\CodeLite"
-LinkerName             :=C:/TDM-GCC-64/bin/g++.exe 
-SharedObjectLinkerName :=C:/TDM-GCC-64/bin/g++.exe -shared -fPIC
+LinkerName             :=C:/WinBuilds/bin/gcc.exe 
+SharedObjectLinkerName :=C:/WinBuilds/bin/gcc.exe -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
 PreprocessSuffix       :=.i
@@ -28,7 +28,7 @@ LibraryPathSwitch      :=-L
 PreprocessorSwitch     :=-D
 SourceSwitch           :=-c 
 OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
-Preprocessors          :=
+Preprocessors          :=$(PreprocessorSwitch)NDEBUG 
 ObjectSwitch           :=-o 
 ArchiveOutputSwitch    := 
 PreprocessOnlySwitch   :=-E
@@ -36,26 +36,26 @@ ObjectsFileList        :="RigolCsvToWav.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=makedir
 RcCmpOptions           := 
-RcCompilerName         :=C:/TDM-GCC-64/bin/windres.exe 
+RcCompilerName         :=C:/WinBuilds/bin/windres.exe 
 LinkOptions            :=  
-IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch)C:/cygwin64/home/sbusnelli/progetti_c/libsndfile/include 
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch)C:/cygwin64/home/sbusnelli/progetti_c/libsndfile64/include 
 IncludePCH             := 
 RcIncludePath          := 
 Libs                   := $(LibrarySwitch)sndfile-1 
 ArLibs                 :=  "libsndfile-1" 
-LibPath                := $(LibraryPathSwitch)C:/cygwin64/home/sbusnelli/progetti_c/libsndfile/bin 
+LibPath                := $(LibraryPathSwitch)C:/cygwin64/home/sbusnelli/progetti_c/libsndfile64/bin 
 
 ##
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := C:/TDM-GCC-64/bin/ar.exe rcu
-CXX      := C:/TDM-GCC-64/bin/g++.exe 
-CC       := C:/TDM-GCC-64/bin/gcc.exe 
-CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
-CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+AR       := C:/WinBuilds/bin/ar.exe rcu
+CXX      := C:/WinBuilds/bin/g++.exe 
+CC       := C:/WinBuilds/bin/gcc.exe 
+CXXFLAGS :=  -O2 -Wall $(Preprocessors)
+CFLAGS   :=  -O2 -Wall $(Preprocessors)
 ASFLAGS  := 
-AS       := C:/TDM-GCC-64/bin/as.exe 
+AS       := C:/WinBuilds/bin/as.exe 
 
 
 ##
@@ -81,7 +81,7 @@ $(OutputFile): $(IntermediateDirectory)/.d $(Objects)
 	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
 
 $(IntermediateDirectory)/.d:
-	@$(MakeDirCommand) "./Debug"
+	@$(MakeDirCommand) "./Release64"
 
 PreBuild:
 
@@ -103,6 +103,6 @@ $(IntermediateDirectory)/main.c$(PreprocessSuffix): main.c
 ## Clean
 ##
 clean:
-	$(RM) -r ./Debug/
+	$(RM) -r ./Release64/
 
 
